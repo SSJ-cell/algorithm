7ab0f5c1dd2b91525edd26a1a7adf070fa1c862b
seongjun <seongjun@example.com>
2026-09-24 20:18:04 +0900

    두번째 커밋
    n <= 1 로 수정.
    그리고 반환값을 n 으로 수정하여 조건문이 더 많아짐을 방지.

--- Week2/1.6.fib1.py
+++ Week2/1.6.fib1.py
@@ -2,9 +2,7 @@
 # student id: 2020103461  
 def fib1(n: int) -> int:
     # Complete the code here
-    if n == 0 :
-        return 0
-    if n == 1 :
-        return 1
-
-    return fib1(n-1) + fib1(n-2)
+    if n <= 1 :
+        return n
+    else :
+        return fib1(n-1) + fib1(n-2)
