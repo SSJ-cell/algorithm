9bdd7c9e8d3a82f2f02744f7e83c79fb96af68cc
seongjun <seongjun@example.com>
2026-09-24 19:46:27 +0900

    첫번째 문제 풀이

--- Week2/1.4.matrixmult.py
+++ Week2/1.4.matrixmult.py
@@ -3,12 +3,9 @@
 from typing import List
 
 def matrixmult(n: int, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-    C = [[0] * n for _ in range(n)]
-
-    # Complete the code here
-    for i in range(n) :
-        for j in range(n) :
-            for k in range(n) :
-                C[i][j] += A[i][k]*B[k][j]
-    
-    return C
+        U = [[0] * n for i in range(n)]
+        for i in range(n) :
+            for j in range(n) :
+                  for k in range(n) :
+                        U[i][j] = A[i][k] * B[k][j]
+        return U
