1c4fe69b9e351ccc81fc9d36ba9aa00fdf079d02
seongjun <seongjun@example.com>
2026-09-24 20:50:54 +0900

    첫번째 시도: 성공

--- Week3/2.1.location.py
+++ Week3/2.1.location.py
@@ -1,14 +1,13 @@
 from typing import List
 
 def location(low: int, high: int, S: List[int], x: int) -> int:
-    if low > high:
+    if low > high :
         return -1
-    else:
-        # Complete the code here
-        mid = (low + high)//2
-        if S[mid] == x :
-            return mid
-        elif S[mid] < x :
-            return location(mid+1, high, S, x)
-        elif S[mid] > x :
-            return location(low, mid-1, S, x)
+    
+    mid = (low + high) // 2
+    if S[mid] == x :
+        return mid
+    elif S[mid] < x :
+        return location(mid + 1, high, S, x)
+    else :
+        return location(low, mid - 1, S, x)
