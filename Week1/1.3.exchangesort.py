19fc53bccb832b31301f241fddd69d05882212c9
seongjun <seongjun@example.com>
2026-09-24 19:00:30 +0900

    i에 가장 작은 원소가 오게끔

--- Week1/1.3.exchangesort.py
+++ Week1/1.3.exchangesort.py
@@ -1,2 +1,7 @@
 from typing import List
 
+def exchangesort(n: int, S: List[int]) :
+    for i in range(n) :
+        for j in range(i+1, n) :
+            if S[j] < S[i] :
+                S[i], S[j] = S[j], S[i]
