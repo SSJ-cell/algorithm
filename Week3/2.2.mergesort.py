d01bec663d8a15e976550e9c932aa5d1a0674b1e
seongjun <seongjun@example.com>
2026-09-24 21:13:22 +0900

    첫번째 시도: 실패

--- Week3/2.2.mergesort.py
+++ Week3/2.2.mergesort.py
@@ -1,32 +1,30 @@
 from typing import List
 
-def merge(h: int, m: int, U: List[int], V: List[int], S: List[int]) -> None:
+def merge(h: int, m: int, U: List[int], V: List[int], S: List[int]) :
     assert sorted(U) == U
     assert sorted(V) == V
-    
+
     i = j = k = 0
-    # Complete the code here
-    while i < h and j < m :
+    while i < h or j < m :
         if U[i] < V[j] :
             S[k] = U[i]
             i += 1
-        else :
+        elif V[j] < U[i] :
             S[k] = V[j]
             j += 1
         k += 1
-    if i == h :
+    if i >= h :
         S[k:] = V[j:]
-    elif j == m :
+    elif j >= m :
         S[k:] = U[i:]
-    
 
-def mergesort(n: int, S: List[int]) -> None:
+def mergesort(n: int, S: List[int]) :
     h = n // 2
     m = n - h
-    if n > 1:
-        # Complete the code here
-        U = S[:h]
-        V = S[h:]
+    U = S[:h]
+    V = S[h+1:]
+
+    if n > 1 :
         mergesort(h, U)
         mergesort(m, V)
         merge(h, m, U, V, S)
