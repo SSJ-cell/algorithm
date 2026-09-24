13a4d89a8b09accfa92b213abad998df2d1dbaa4
seongjun <seongjun@example.com>
2026-09-24 19:51:02 +0900

    두번째 커밋(언더바, += 수정)
    for문에서 언더바 사용은 횟수만 채우고 싶을 때 사용함
    행렬의 연산이므로 += 로 수정하였음. 기존 코드대로 하면 마지막 곱연산만 U[i][j]에 덮어씌워지는 것임.

--- Week2/1.4.matrixmult.py
+++ Week2/1.4.matrixmult.py
@@ -3,9 +3,9 @@
 from typing import List
 
 def matrixmult(n: int, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-        U = [[0] * n for i in range(n)]
+        U = [[0] * n for _ in range(n)]
         for i in range(n) :
             for j in range(n) :
                   for k in range(n) :
-                        U[i][j] = A[i][k] * B[k][j]
+                        U[i][j] += A[i][k] * B[k][j]
         return U
