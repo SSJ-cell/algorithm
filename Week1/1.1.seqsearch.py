44e0a6e197a3caa510c7fc67032bb9a539da5c69
seongjun <seongjun@example.com>
2026-09-24 18:30:08 +0900

    이전 커밋에 설명 추가
    location < n 조건문을 먼저 써야 함. S[location] != x를 먼저 쓰게 되면 location == n 이 될 때 리스트의 인덱스를 넘어가게 되는 오류 발생
    
    for문 대신 while문으로 작성함. for은 찾아도 모든 요소를 순회함. 반면 while문은 찾으면 순회를 멈추도록 할 수 있음

--- Week1/1.1.seqsearch.py
+++ Week1/1.1.seqsearch.py
@@ -1,12 +1,11 @@
 from typing import List
 
-
 def seqsearch(n: int, S: List[int], x: int) -> int:
     location = 0
 
     while location < n and S[location] != x:
         location += 1
+    if location >= n:
+        location = -1
 
-    if location < n:
-        return location
-    return -1
+    return location
