7d81bddaa05cab28eec141e26b2db45c3dd51179
seongjun <seongjun@example.com>
2026-09-25 12:55:04 +0900

    두번째 시도: 무한루프?

--- Week3/2.4.mergesort2.py
+++ Week3/2.4.mergesort2.py
@@ -1,26 +1,26 @@
 from typing import List
 
 def merge2(low: int, mid: int, high: int, S: List[int]) :
-        U = [0]*high
+        U = [0]*(high + 1)
         i, j, k = low, mid+1, 0
 
         while i <= mid and j <= high :
             if S[i] <= S[j] :
                 U[k] = S[i]
                 i +=1
-            elif S[j] <= S[i] :
+            elif S[j] < S[i] :
                 U[k] = S[j]
                 j += 1
             k += 1
         if i > mid :
-            U[k:] = S[j:high]
+            U[k:] = S[j:high+1]
         elif j > high :
-            U[k:] = S[low:mid+1]   
-        S[low:high] = U[:]
+            U[k:] = S[i:mid+1]   
+        S[low:high+1] = U[:]
 
 
 def mergesort2(low: int, high: int, S: List[int]) :
-        if high > 0 :   
+        if high > low :   
             mid = (low + high) // 2
             mergesort2(low, mid, S)
             mergesort2(mid+1, high, S)
