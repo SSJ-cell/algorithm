9bdad19c2462b52bcdde72ee67f11e7f8a9f502f
seongjun <seongjun@example.com>
2026-09-26 20:58:35 +0900

    두번째 커밋
    -기존 코드는 n^2 시간복잡도
    -현재 코드는 nlog(n) 시간복잡도
    
    정렬된 nums에서 리스트 요소의 인덱스 값이 해당 요소 보다 작은 값을 나타냄을 활용.
    그래서 그 인덱스와 값을 딕셔너리에서 값과 키로 활용함.

--- Leet/1365-HowManyNumberAreSmallerThantheCurrentNumber.py
+++ Leet/1365-HowManyNumberAreSmallerThantheCurrentNumber.py
@@ -2,11 +2,11 @@
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
-        result = []
-        for i in range(len(nums)) :
-            count = 0
-            for j in range(len(nums)) :
-                if nums[i] > nums[j] :
-                    count += 1
-            result.append(count)
-        return result
+        sorted_nums = sorted(nums)
+        rank = {}
+
+        for i, num in enumerate(sorted_nums) :
+            if num not in rank :
+                rank[num] = i
+
+        return [rank[num] for num in nums]
